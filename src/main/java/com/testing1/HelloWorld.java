dabc54fe19754e76b28a918eb8d695189bf526ef
longi <devitlongi@gmail.com>
2017-05-10 18:00:39 +0200

    prvy pokus

---
+++ src/main/java/com/testing1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.testing1;
+
+/**
+ * Created by longi on 10.5.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("naydar Joyko");
+    }
+}
